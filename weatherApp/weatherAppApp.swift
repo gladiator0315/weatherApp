480ae20a21b283625deee2a1d4848643496ca0cd
Calvin Nary <calvinnary@student.bend.k12.or.us>
2022-05-19 13:08:12 -0700

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Calvin Nary on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
